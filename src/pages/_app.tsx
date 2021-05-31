8c6334e78e65e447eca65583acbc2eee33e829ef
Matheus8174 <souzamatheus8174@gmail.com>
2021-05-31 11:35:28 -0300

    feat: update _app.tsx add pwa suport

---
+++ src/pages/_app.tsx
@@ -0,0 +1,24 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>React Avançado - Boilerplate</title>
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#06092B" />
+        <meta
+          name="description"
+          content="A simple project to practice my knowledge of the nextJS, stylized-components, jest and eslint"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default App;
